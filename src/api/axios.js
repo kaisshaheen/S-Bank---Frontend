4563ae087f5a6b92b37603026eb0d96c5c28e0c9
Kais Shaheen <kaeessh22@gmail.com>
2026-05-14 18:42:56 +0300

    editing axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,13 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://s-bank-backend-production.up.railway.app",
     withCredentials: true,
+    withXSRFToken: true,
+
+    headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
 });
 
 // 🔥 THIS IS THE MISSING PIECE
