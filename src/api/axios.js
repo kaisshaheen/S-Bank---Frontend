6a7907a62c9f59afe37e9b9dd362e7b141b403ed
Kais Shaheen <kaeessh22@gmail.com>
2026-05-14 18:51:58 +0300

    editing axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,6 +5,9 @@ const api = axios.create({
     withCredentials: true,
     withXSRFToken: true,
 
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
+
     headers: {
         "X-Requested-With": "XMLHttpRequest",
         "Accept": "application/json",
